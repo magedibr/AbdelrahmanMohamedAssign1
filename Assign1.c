08f5a39797dd5b180cd9d5e8ace43417f474b4e9
magedibr <59849099+magedibr@users.noreply.github.com>
2020-10-02 17:04:55 -0400

    Done almost everything

--- Assign1.c
+++ Assign1.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "Assign1.h"
 #include "suduko.h"
-
+#define BUFFER_SIZE 100
 
 
 
@@ -12,19 +12,26 @@
 
 int main() {
 int i,j,k,l;
+l=0;
+int arr[9][9];
+
+int* col1 ;
+int* col2;
+
+col1= input();
+col2= input();
 
 
-    int arr[9][9];
 
-int *col1 = input();
-int* col2 = input();
+    for(int q=1;q<10;q++){
+     //   arr[0][q]=*col1+q;
+      //  printf(" Col 1 %d: ", col1[q]);
+        arr[0][q-1] = col1[q];
+        arr[1][q-1] = col2[q];
+     //   printf("%d",*col2+q);
+        //arr[1][q]=*(col2+q);
 
-
-    for(int q=0;q<9;q++){
-        arr[0][q]=*col1+q;
-       printf("%d", *col1);
-        arr[1][q]=*(col2+q);
-        printf("%d", *(col2));
+        //printf("%d", *(col2));
         //col1++;
     }
 
@@ -68,24 +75,61 @@ printFinal(arr);
 
 }
 
+static int arr[9];
+static int arr2[9];
+int track=0;
+char buffer[BUFFER_SIZE];
 
 
+int* input() {
+    ++track;
+    printf("Track = %d ",track);
+    if(track>1) {
+        int ctr = 0;
+        while (ctr != 9) {
+            fgets(buffer, sizeof(buffer), stdin);
+            ctr = sscanf(buffer, "%d%d%d%d%d%d%d%d%d", arr2 + 1, arr2 + 2, arr2 + 3, arr2 + 4, arr2 + 5, arr2 + 6, arr2 + 7,
+                         arr2 + 8, arr2 + 9);
+            if (ctr != 9) {
+                puts(
+                        "\nEnter exactly INTIGERS separated by Space/Tab followed by Enter key to end!\n"
+                        "Use Space/Tab/Return key to enter the next values\n"
+                        "and press 'Return' after entering nine integers.\n"
+                        "Remember, to make a valid row of Sudoku gird,\n"
+                        "all integers should be distinct and range from 1 to 9\n");
+                ctr = 0;
+                continue;
+            }
+        }
+        //Recursion is used for loop till user input is fully validated
+        if (dup(arr2) == 1 || range(arr2) == 1) return input();printf("%p\n",arr);
+        return arr2;
 
+    } else {
 
+        int ctr = 0;
+        while (ctr != 9) {
+            fgets(buffer, sizeof(buffer), stdin);
+            ctr = sscanf(buffer, "%d%d%d%d%d%d%d%d%d", arr + 1, arr + 2, arr + 3, arr + 4, arr + 5, arr + 6, arr + 7,
+                         arr + 8, arr + 9);
+            if (ctr != 9) {
+                puts(
+                        "\nEnter exactly INTIGERS separated by Space/Tab followed by Enter key to end!\n"
+                        "Use Space/Tab/Return key to enter the next values\n"
+                        "and press 'Return' after entering nine integers.\n"
+                        "Remember, to make a valid row of Sudoku gird,\n"
+                        "all integers should be distinct and range from 1 to 9\n");
+                ctr = 0;
+                continue;
+            }
+        }
+        //Recursion is used for loop till user input is fully validated
+        if (dup(arr) == 1 || range(arr) == 1) return input();
+        return arr;
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
--- implementation.c
+++ implementation.c
@@ -2,37 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUFFER_SIZE 100
 
 
-static int arr[9];
-char buffer[BUFFER_SIZE];
+//static array so that it is not destroyed after function is called
 
-
-int* input() {
-
-    int ctr =0;
-    while(ctr != 9){
-        fgets(buffer, sizeof(buffer), stdin);
-        ctr = sscanf(buffer, "%d%d%d%d%d%d%d%d%d", arr+1,arr+2,arr+3,arr+4,arr+5,arr+6,arr+7,arr+8,arr+9);
-        if(ctr != 9){
-            puts(
-                    "\nEnter exactly INTIGERS separated by Space/Tab followed by Enter key to end!\n"
-                    "Use Space/Tab/Return key to enter the next values\n"
-                    "and press 'Return' after entering nine integers.\n"
-                    "Remember, to make a valid row of Sudoku gird,\n"
-                    "all integers should be distinct and range from 1 to 9\n");
-            ctr =0;
-            continue;
-        }
-    }
-    //Recursion is used for loop till user input is fully validated
-    if(dup(arr)==1 || range(arr)==1) return input();
-  return arr;
-
-
-
-}
 
 int dup(int arr[]){
     int i;
