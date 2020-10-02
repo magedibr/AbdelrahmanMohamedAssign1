84a70f7627bc410289854fd4e66499798b991ddf
magedibr <59849099+magedibr@users.noreply.github.com>
2020-10-02 17:06:03 -0400

    Done almost everything

--- Assign1.c
+++ Assign1.c
@@ -75,62 +75,6 @@ printFinal(arr);
 
 }
 
-static int arr[9];
-static int arr2[9];
-int track=0;
-char buffer[BUFFER_SIZE];
-
-
-int* input() {
-    ++track;
-    printf("Track = %d ",track);
-    if(track>1) {
-        int ctr = 0;
-        while (ctr != 9) {
-            fgets(buffer, sizeof(buffer), stdin);
-            ctr = sscanf(buffer, "%d%d%d%d%d%d%d%d%d", arr2 + 1, arr2 + 2, arr2 + 3, arr2 + 4, arr2 + 5, arr2 + 6, arr2 + 7,
-                         arr2 + 8, arr2 + 9);
-            if (ctr != 9) {
-                puts(
-                        "\nEnter exactly INTIGERS separated by Space/Tab followed by Enter key to end!\n"
-                        "Use Space/Tab/Return key to enter the next values\n"
-                        "and press 'Return' after entering nine integers.\n"
-                        "Remember, to make a valid row of Sudoku gird,\n"
-                        "all integers should be distinct and range from 1 to 9\n");
-                ctr = 0;
-                continue;
-            }
-        }
-        //Recursion is used for loop till user input is fully validated
-        if (dup(arr2) == 1 || range(arr2) == 1) return input();printf("%p\n",arr);
-        return arr2;
-
-    } else {
-
-        int ctr = 0;
-        while (ctr != 9) {
-            fgets(buffer, sizeof(buffer), stdin);
-            ctr = sscanf(buffer, "%d%d%d%d%d%d%d%d%d", arr + 1, arr + 2, arr + 3, arr + 4, arr + 5, arr + 6, arr + 7,
-                         arr + 8, arr + 9);
-            if (ctr != 9) {
-                puts(
-                        "\nEnter exactly INTIGERS separated by Space/Tab followed by Enter key to end!\n"
-                        "Use Space/Tab/Return key to enter the next values\n"
-                        "and press 'Return' after entering nine integers.\n"
-                        "Remember, to make a valid row of Sudoku gird,\n"
-                        "all integers should be distinct and range from 1 to 9\n");
-                ctr = 0;
-                continue;
-            }
-        }
-        //Recursion is used for loop till user input is fully validated
-        if (dup(arr) == 1 || range(arr) == 1) return input();
-        return arr;
-
-    }
-
-}
-
 
 
 
